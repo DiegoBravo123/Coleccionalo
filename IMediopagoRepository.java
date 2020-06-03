5a5c88d18806836b4c32b94d38903b3879851a6a
DiegoBravo123 <55288027+DiegoBravo123@users.noreply.github.com>
2020-06-03 16:57:18 -0500

    Add files via upload

---
+++ IMediopagoRepository.java
@@ -0,0 +1,10 @@
+package pe.edu.upc.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import pe.edu.upc.entity.Mediopago;
+
+
+public interface IMediopagoRepository extends JpaRepository<Mediopago, Integer>{
+
+}
