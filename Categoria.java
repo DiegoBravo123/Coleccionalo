49460dd2e1906d8f74752d11130ad000e5905375
DiegoBravo123 <55288027+DiegoBravo123@users.noreply.github.com>
2020-06-03 23:01:58 -0500

    Add files via upload

---
+++ Categoria.java
@@ -0,0 +1,39 @@
+package pe.edu.upc.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name = "categorias")
+public class Categoria implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idCategoria;
+	
+	private String desCategoria;
+
+	public int getIdCategoria() {
+		return idCategoria;
+	}
+
+	public void setIdCategoria(int idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+
+	public String getDesCategoria() {
+		return desCategoria;
+	}
+
+	public void setDesCategoria(String desCategoria) {
+		this.desCategoria = desCategoria;
+	}
+	
+	
+}
